3007ded109277e48b339f93c6524c74cf61cd114
gupettine <gupettine@gmail.com>
2024-06-24 11:23:21 -0300

    feature: create transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
+import {
+  TransactionsContainer,
+  TransactionsTable,
+  PriceHighlight,
+} from './styles'
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Website development</td>
+              <td>
+                <PriceHighlight variant="income">$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Sale</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Apartment rent</td>
+              <td>
+                <PriceHighlight variant="outcome">- $ 2.000,00</PriceHighlight>
+              </td>
+              <td>Expense</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
