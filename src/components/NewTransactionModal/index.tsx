20dda1ad1d19fde0d7c495ca467284b5583fe109
gupettine <gupettine@gmail.com>
2024-06-24 21:12:54 -0300

    feature: create input and output buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,13 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
-import { Overlay, Content, CloseButton } from './styles'
+import {
+  Overlay,
+  Content,
+  CloseButton,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -19,6 +25,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Description" required />
           <input type="number" placeholder="Price" required />
           <input type="text" placeholder="Category" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant='income'>
+              <ArrowCircleUp size={24} />
+              Input
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome'>
+              <ArrowCircleDown size={24} />
+              Output
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Register</button>
         </form>
